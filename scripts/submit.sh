fbe0f6df45ceb50ca302b17b888c4f7017b2b3a5
Chris Barrick <cbarrick1@gmail.com>
2018-02-06 18:21:52 -0500

    Fix default cluster name in submit.sh

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,7 +3,7 @@
 ./setup.py bdist_egg
 
 gcloud dataproc jobs submit pyspark \
-	--cluster hydrus \
+	--cluster elizabeth \
 	--region us-east1 \
 	--py-files ./dist/elizabeth-0.1.0.dev0-py3.6.egg \
 	./scripts/driver.py \
