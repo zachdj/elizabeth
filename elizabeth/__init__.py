3500071199d3cfd019962b940c8c0b52daed130b
Chris Barrick <cbarrick1@gmail.com>
2018-02-10 14:30:42 -0500

    Add function to get a SparkSession

--- elizabeth/__init__.py
+++ elizabeth/__init__.py
@@ -9,3 +9,14 @@ def context(**kwargs):
     conf.setAll(kwargs.items())
     ctx = pyspark.SparkContext()
     return ctx
+
+
+def session(**kwargs):
+    conf = pyspark.SparkConf()
+    conf.setAppName('elizabeth')
+    conf.setAll(kwargs.items())
+    sess = (pyspark.sql.SparkSession
+        .builder
+        .config(conf=conf)
+        .getOrCreate())
+    return sess
