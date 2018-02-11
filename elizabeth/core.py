45feac6735fa3bd9bdffdf4679abed4aef2d7565
Chris Barrick <cbarrick1@gmail.com>
2018-02-10 20:15:44 -0500

    Move helpers into new module elizabeth.core

---
+++ elizabeth/core.py
@@ -0,0 +1,90 @@
+import pyspark
+
+
+def session(**kwargs):
+    '''Get or create the global `SparkSession`.
+
+    Kwargs:
+        Forwarded to `SparkConf.setAll` to initialize the session.
+
+    Returns:
+        SparkSession
+    '''
+    conf = pyspark.SparkConf()
+    conf.setAppName('elizabeth')
+    conf.setAll(kwargs.items())
+    sess = (pyspark.sql.SparkSession
+        .builder
+        .config(conf=conf)
+        .getOrCreate())
+    return sess
+
+
+def data_type(dtype, not_null=False):
+    '''Convert `dtype` to a valid `pyspark.sql.types.DataType`.
+
+    Args:
+        dtype:
+            Some indicator of the type. It may be
+            - an existing `DataType`,
+            - a string naming the type,
+            - a Python type, like `str`,
+            - or a list containing exactly one element,
+              interpreted as an `ArrayType` of the given the element type.
+        not_null (bool):
+            Should values be allowed to be null.
+            This is most applicable for `ArrayType`s.
+
+    Returns:
+        pyspark.sql.types.DataType
+    '''
+    types = {
+        'null': pyspark.sql.types.NullType(),
+        'string': pyspark.sql.types.StringType(),
+        'binary': pyspark.sql.types.BinaryType(),
+        'boolean': pyspark.sql.types.BooleanType(),
+        'date': pyspark.sql.types.DateType(),
+        'timestamp': pyspark.sql.types.TimestampType(),
+        'double': pyspark.sql.types.DoubleType(),
+        'float': pyspark.sql.types.FloatType(),
+        'byte': pyspark.sql.types.ByteType(),
+        'integer': pyspark.sql.types.IntegerType(),
+        'long': pyspark.sql.types.LongType(),
+        'short': pyspark.sql.types.ShortType(),
+
+        None: pyspark.sql.types.NullType(),
+        str: pyspark.sql.types.StringType(),
+        bool: pyspark.sql.types.BooleanType(),
+        float: pyspark.sql.types.DoubleType(),
+        int: pyspark.sql.types.LongType(),
+    }
+
+    # If dtype is a list, interpret it as an ArrayType.
+    if isinstance(dtype, list):
+        assert len(dtype) == 1
+        element_type = data_type(dtype[0])
+        dtype = pyspark.sql.types.ArrayType(element_type, containsNull=(not not_null))
+
+    # If dtype is not a DataType, then look it up in the table.
+    if not isinstance(dtype, pyspark.sql.types.DataType):
+        dtype = types[dtype]
+
+    assert isinstance(dtype, pyspark.sql.types.DataType)
+    assert not not_null or dtype != pyspark.sql.types.NullType
+    return dtype
+
+
+def udf(dtype):
+    '''A decorator for Spark User Defined Functions (UDF).
+
+    UDFs are used to apply transforms to DataFrames.
+
+    Args:
+        dtype:
+            The return type of the UDF, interpreted by `elizabeth.data_type`.
+
+    Returns:
+        A decorator which converts a function to a `pyspark.sql.functions.udf`.
+    '''
+    dtype = data_type(dtype)
+    return lambda f: pyspark.sql.functions.udf(f, dtype)
