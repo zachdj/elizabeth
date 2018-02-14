94839934c16eaf6c2250e5907fb7662c05488768
Zach Jones <zachydj@gmail.com>
2018-02-13 23:50:05 -0500

    Index labels before nb classification

--- elizabeth/naive_bayes.py
+++ elizabeth/naive_bayes.py
@@ -1,16 +1,26 @@
 from pyspark.sql.functions import avg
 from pyspark.ml.classification import NaiveBayes
-from pyspark.ml.feature import CountVectorizer, IDF, NGram
+from pyspark.ml.feature import CountVectorizer, IDF, NGram, StringIndexer, IndexToString
 
 import elizabeth
 
 
 def main(train_x, train_y, test_x, test_y=None, idf=False, ngram=1, base='gs', asm=False):
-    # Load : DF[id, url, text, features, label?]
     # The DataFrames only have a labels column if labels are given.
     kind = 'asm' if asm else 'bytes'
     train = elizabeth.load(train_x, train_y, base=base, kind=kind)
     test = elizabeth.load(test_x, test_y, base=base, kind=kind)
+
+    # convert the string labels to numeric indices
+    # the handleInvalid param allows the label indexer to deal with labels that weren't seen during fitting
+    label_indexer = StringIndexer(inputCol='label', outputCol='indexedLabel', handleInvalid="error")
+    label_indexer = label_indexer.fit(train)
+    train = label_indexer.transform(train)
+    # the test set won't always have labels
+    if test_y is not None:
+        test = label_indexer.transform(test)
+
+    index_labeller = IndexToString(inputCol='prediction', outputCol='predictedClass', labels=label_indexer.labels)
 
     # Train the preprocessor and transform the data.
     prep = elizabeth.Preprocessor()
@@ -21,20 +31,20 @@ def main(train_x, train_y, test_x, test_y=None, idf=False, ngram=1, base='gs', a
     test = prep.transform(test)
 
     # Naive Bayes : DF[id, url, text, features, label?, rawPrediction, probability, prediction]
-    nb = NaiveBayes().fit(train)
+    nb = NaiveBayes(labelCol='indexedLabel').fit(train)
     test = nb.transform(test)
-    test = test.withColumn('prediction', test.prediction + 1)
+    test = index_labeller.transform(test)  # DF[id, url, ... prediction, predictedClass]
 
-    # If labels are given for the test set, print a score.
+    # If labels are given for the test set, print a score.s
     if test_y:
         test = test.orderBy(test.id)
-        test = test.withColumn('correct', (test.label == test.prediction).cast('double'))
+        test = test.withColumn('correct', (test.label == test.predictedClass).cast('double'))
         test = test.select(avg(test.correct))
         print(test.show())
 
     # If no labels are given for the test set, print predictions.
     else:
-        test = test.orderBy(test.id).select(test.prediction)
-        test = test.rdd.map(lambda row: int(row.prediction))
+        test = test.orderBy(test.id).select(test.predictedClass)
+        test = test.rdd.map(lambda row: int(row.predictedClass))
         test = test.toLocalIterator()
         print(*test, sep='\n')
