2256af133a084650125e753af99b168c7e2a26f7
Zach Jones <zachydj@gmail.com>
2018-02-15 04:35:02 -0500

    Incorporate asm features in RF classifier

--- elizabeth/tree_ensemble.py
+++ elizabeth/tree_ensemble.py
@@ -2,32 +2,50 @@
     Random Forest classifier
 
     Features:
-        - frequencies of the top 500 bigrams from the binary files
-        - frequencies of the top 1000 4-grams from the binary files
+        - lengths of different sections (HEADER, .text, .idata, .rsrc, etc) of asm file
+        - frequency of opcodes
+        - frequencies of the top 30 bigrams from the binary files
+        - frequencies of the top 100 4-grams from the binary files
 """
 
 from pyspark.sql.functions import avg
 from pyspark.ml.classification import RandomForestClassifier, OneVsRest
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-from pyspark.ml.feature import VectorAssembler, CountVectorizer, NGram, StringIndexer, IndexToString
+from pyspark.ml.feature import VectorAssembler, CountVectorizer, NGram, StringIndexer, IndexToString, OneHotEncoder
 
 import elizabeth
 
 
 def main(train_x, train_y, test_x, test_y=None, base='gs'):
-    # Load : DF[id, url, features, label?]
-    # The DataFrames only have a labels column if labels are given.
-    train = elizabeth.load(train_x, train_y, base=base, kind='bytes').drop('text')
-    test = elizabeth.load(test_x, test_y, base=base, kind='bytes').drop('text')
+    # generate feature set for asm files
+    train_asm_features = elizabeth.preprocess.load_asm_tree_features(train_x).drop('url')
+    test_asm_features = elizabeth.preprocess.load_asm_tree_features(test_x).drop('url')
+
+    section_cv = CountVectorizer(inputCol='sections', outputCol='section_counts').fit(train_asm_features)
+    train_asm_features = section_cv.transform(train_asm_features).drop('sections')
+    test_asm_features = section_cv.transform(test_asm_features).drop('sections')
+
+    opcode_cv = CountVectorizer(inputCol='opcodes', outputCol='opcode_counts').fit(test_asm_features)
+    train_asm_features = opcode_cv.transform(train_asm_features).drop('opcodes')
+    test_asm_features = opcode_cv.transform(test_asm_features).drop('opcodes')
+
+    asm_va = VectorAssembler(inputCols=['section_counts', 'opcode_counts'], outputCol='asm_features')
+
+    train_asm_features = asm_va.transform(train_asm_features)
+    test_asm_features = asm_va.transform(test_asm_features)
+
+    # generate feature set for byte files
+    train_byte_features = elizabeth.load(train_x, train_y, base=base, kind='bytes').drop('text')
+    test_byte_features = elizabeth.load(test_x, test_y, base=base, kind='bytes').drop('text')
 
     # convert the string labels to numeric indices
     # the handleInvalid param allows the label indexer to deal with labels that weren't seen during fitting
     label_indexer = StringIndexer(inputCol='label', outputCol='indexedLabel', handleInvalid="skip")
-    label_indexer = label_indexer.fit(train)
-    train = label_indexer.transform(train)
+    label_indexer = label_indexer.fit(train_byte_features)
+    train_byte_features = label_indexer.transform(train_byte_features)
     # the test set won't always have labels
     if test_y is not None:
-        test = label_indexer.transform(test)
+        test_byte_features = label_indexer.transform(test_byte_features)
 
     index_labeller = IndexToString(inputCol='prediction', outputCol='predictedClass', labels=label_indexer.labels)
 
@@ -35,36 +53,44 @@ def main(train_x, train_y, test_x, test_y=None, base='gs'):
     # create ngrams
     two_gram = NGram(n=2, inputCol='features', outputCol='twoGrams')
     four_gram = NGram(n=4, inputCol='features', outputCol='fourGrams')
-    train = two_gram.transform(train)
-    train = four_gram.transform(train)
-    test = two_gram.transform(test)
-    test = four_gram.transform(test)
+    train_byte_features = two_gram.transform(train_byte_features)
+    train_byte_features = four_gram.transform(train_byte_features)
+    test_byte_features = two_gram.transform(test_byte_features)
+    test_byte_features = four_gram.transform(test_byte_features)
 
     # create ngram frequencies
-    cv = CountVectorizer(inputCol="twoGrams", outputCol="twoGramCounts", vocabSize=500)
-    cv_model = cv.fit(train)
-    train = cv_model.transform(train).drop('twoGrams')
-    test = cv_model.transform(test).drop('twoGrams')
+    cv = CountVectorizer(inputCol="twoGrams", outputCol="byte_features", vocabSize=30)
+    cv_model = cv.fit(train_byte_features)
+    train_byte_features = cv_model.transform(train_byte_features).drop('twoGrams')
+    test_byte_features = cv_model.transform(test_byte_features).drop('twoGrams')
 
-    cv = CountVectorizer(inputCol="fourGrams", outputCol="fourGramCounts", vocabSize=1000)
-    cv_model = cv.fit(train)
-    train = cv_model.transform(train).drop('fourGrams')
-    test = cv_model.transform(test).drop('fourGrams')
+    # cv = CountVectorizer(inputCol="fourGrams", outputCol="fourGramCounts", vocabSize=100)
+    # cv_model = cv.fit(train_byte_features)
+    # train_byte_features = cv_model.transform(train_byte_features).drop('fourGrams')
+    # test_byte_features = cv_model.transform(test_byte_features).drop('fourGrams')
 
     # drop the tokens loaded by the preprocessor
-    train = train.drop('features')
-    test = test.drop('features')
+    train_byte_features = train_byte_features.drop('features')
+    test_byte_features = test_byte_features.drop('features')
 
     # combine features into a single feature vector, and drop the individual columns
-    assembler = VectorAssembler(inputCols=['twoGramCounts', 'fourGramCounts'], outputCol='features')
-    train = assembler.transform(train)\
-        .drop('twoGramCounts', 'fourGramCounts')
-    test = assembler.transform(test)\
-        .drop('twoGramCounts', 'fourGramCounts')
+    # bytes_va = VectorAssembler(inputCols=['twoGramCounts', 'fourGramCounts'], outputCol='byte_features')
+    # train_byte_features = bytes_va.transform(train_byte_features)\
+    #     .drop('twoGramCounts', 'fourGramCounts')
+    # test_byte_features = bytes_va.transform(test_byte_features)\
+    #     .drop('twoGramCounts', 'fourGramCounts')
 
-    # instantiate a binary GBT as the base classifier for One-vs-Many
+    # unify the features from the bytes and the asm files
+    train = train_asm_features.join(train_byte_features, on='id')
+    test = test_asm_features.join(test_byte_features, on='id')
+
+    assembler = VectorAssembler(inputCols=['byte_features', 'asm_features'], outputCol='features')
+    train = assembler.transform(train).drop('byte_features', 'asm_features')
+    test = assembler.transform(test).drop('byte_features', 'asm_features')
+
+    # create and train a Random Forest classifier
     rf = RandomForestClassifier(labelCol='indexedLabel', featuresCol='features',
-                                 numTrees=20, maxDepth=10, minInfoGain=0.0)
+                                 numTrees=10, maxDepth=5, minInfoGain=0.0)
     model = rf.fit(train)
     prediction = model.transform(test)
     prediction = index_labeller.transform(prediction)  # DF[id, url, ... prediction, predictedClass]
