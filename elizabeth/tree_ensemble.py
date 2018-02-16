d87ff05e271ca58f32aa5deecedd7082a70b3615
Zach Jones <zachydj@gmail.com>
2018-02-16 01:57:43 -0500

    Train on asm features only

--- elizabeth/tree_ensemble.py
+++ elizabeth/tree_ensemble.py
@@ -16,40 +16,12 @@ import elizabeth
 
 def main(train_x, train_y, test_x, test_y=None, base='gs'):
     # generate feature set for asm files
-    train_asm_features = elizabeth.preprocess.load(train_x, train_y, base=base,  kind='sect_ops').drop('url', 'label')
-    test_asm_features = elizabeth.preprocess.load(test_x, test_y, base=base,  kind='sect_ops').drop('url', 'label')
+    train_asm_features = elizabeth.preprocess.load(train_x, train_y, base=base,  kind='sect_ops').drop('url')
+    test_asm_features = elizabeth.preprocess.load(test_x, test_y, base=base,  kind='sect_ops').drop('url')
 
-    asm_token_counter = CountVectorizer(inputCol='features', outputCol='asm_features', vocabSize=45).fit(train_asm_features)
-    train_asm_features = asm_token_counter.transform(train_asm_features).drop('features')
-    test_asm_features = asm_token_counter.transform(test_asm_features).drop('features')
-
-    # generate feature set for byte files
-    train_byte_features = elizabeth.load(train_x, train_y, base=base, kind='bytes').drop('text')
-    test_byte_features = elizabeth.load(test_x, test_y, base=base, kind='bytes').drop('text')
-
-    # transform the data
-    # create ngrams
-    two_gram = NGram(n=2, inputCol='features', outputCol='twoGrams')
-    train_byte_features = two_gram.transform(train_byte_features)
-    test_byte_features = two_gram.transform(test_byte_features)
-
-    # create ngram frequencies
-    cv = CountVectorizer(inputCol="twoGrams", outputCol="byte_features", vocabSize=30)
-    cv_model = cv.fit(train_byte_features)
-    train_byte_features = cv_model.transform(train_byte_features).drop('twoGrams')
-    test_byte_features = cv_model.transform(test_byte_features).drop('twoGrams')
-
-    # drop the tokens loaded by the preprocessor
-    train_byte_features = train_byte_features.drop('features')
-    test_byte_features = test_byte_features.drop('features')
-
-    # unify the features from the bytes and the asm files
-    train = train_asm_features.join(train_byte_features, on='id')
-    test = test_asm_features.join(test_byte_features, on='id')
-
-    assembler = VectorAssembler(inputCols=['byte_features', 'asm_features'], outputCol='features')
-    train = assembler.transform(train).drop('byte_features', 'asm_features')
-    test = assembler.transform(test).drop('byte_features', 'asm_features')
+    asm_token_counter = CountVectorizer(inputCol='features', outputCol='asm_features', minDF=10).fit(train_asm_features)
+    train = asm_token_counter.transform(train_asm_features).drop('features')
+    test = asm_token_counter.transform(test_asm_features).drop('features')
 
     # convert the string labels to numeric indices
     # the handleInvalid param allows the label indexer to deal with labels that weren't seen during fitting
@@ -63,8 +35,8 @@ def main(train_x, train_y, test_x, test_y=None, base='gs'):
     index_labeller = IndexToString(inputCol='prediction', outputCol='predictedClass', labels=label_indexer.labels)
 
     # create and train a Random Forest classifier
-    rf = RandomForestClassifier(labelCol='indexedLabel', featuresCol='features',
-                                 numTrees=10, maxDepth=10, minInfoGain=0.0, seed=12345)
+    rf = RandomForestClassifier(labelCol='indexedLabel', featuresCol='asm_features',
+                                 numTrees=20, maxDepth=10, minInfoGain=0.0, seed=12345)
     model = rf.fit(train)
     prediction = model.transform(test)
     prediction = index_labeller.transform(prediction)  # DF[id, url, ... prediction, predictedClass]
